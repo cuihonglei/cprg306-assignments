a694ffd8adb31c13bf78cc615867bc106263e569
Robin Cui <honglei9423@gmail.com>
2024-01-17 18:04:08 -0700

    Added the week 2 assignment.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <>
+      <div>Honglei Cui</div>
+      <Link href="https://github.com/cuihonglei">https://github.com/cuihonglei</Link>
+    </>
+  )
+}
