cf8eecc9931bb01de529cba728285c07142fe47f
Robin Cui <honglei9423@gmail.com>
2024-02-12 15:31:53 -0700

    Added the week-4 Assignment.

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2" className="hover:text-green-400 hover:underline">Week 2 Assignment</Link>
       <Link href="/week-3" className="hover:text-green-400 hover:underline">Week 3 Assignment</Link>
+      <Link href="/week-4" className="hover:text-green-400 hover:underline">Week 4 Assignment</Link>
     </main>
   )
 }
